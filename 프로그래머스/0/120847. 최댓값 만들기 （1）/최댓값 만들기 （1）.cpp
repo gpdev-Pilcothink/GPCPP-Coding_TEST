26e78a4a4c574b97a2fba2bca41f2b6eb0ebefa9
문경필 <gpieceoffice@gmail.com>
2025-05-28 18:20:08 +0900

    Update 최댓값 만들기 （1）.cpp

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -6,24 +6,22 @@ using namespace std;
 int solution(vector<int> numbers) {
     int max=0;
     int max2=0;
-    int maxi=0;
     
     for(int i=0; i< numbers.size();  i++)
     {
-        if(numbers[i] >= max )
+        if(numbers[i]>max)
         {
-            max=numbers[i];
-            maxi=i;
+            max2 = max;
+            max = numbers[i];
         }
-    };
-
-    for(int i=0; i< numbers.size();  i++)
-    {
-        if(numbers[i]<=max && numbers[i]>=max2 && i!=maxi )
+        else if(numbers[i]>max2)
         {
             max2=numbers[i];
         }
+        
     };
+
+   
     
     int answer = max*max2;
     return answer;
